742944621b4e41f056f2f3b676c6d56d13dffcc3
Sandhj <135236046+Sandhj@users.noreply.github.com>
2023-07-24 17:07:44 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,6 +12,7 @@ rm -rf trial
 rm -rf up
 rm -rf genssl
 rm -rf slow
+rm -rf menu-backup
 # download script
 cd /usr/bin
 # menu
@@ -23,6 +24,7 @@ wget -O slow "https://raw.githubusercontent.com/Andyyuda/ver3/main/slow.sh"
 wget -O usernew "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/usernew.sh"
 wget -O trial "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/trial.sh"
 wget -O menu-ssh "https://raw.githubusercontent.com/Sandhj/v4/main/menu/menu-ssh.sh"
+wget -O menu-backup "https://raw.githubusercontent.com/Sandhj/v4/main/backup/menu-backup.sh"
 
 # chomad
 wget -q -O /usr/bin/up "https://raw.githubusercontent.com/Sandhj/v4/main/up.sh" && chmod +x /usr/bin/up
@@ -30,10 +32,12 @@ wget -q -O /usr/bin/cf "https://raw.githubusercontent.com/Andyyuda/ver3/main/cf.
 wget -q -O /usr/bin/slow "https://raw.githubusercontent.com/Andyyuda/ver3/main/slow.sh" && chmod +x /usr/bin/slow
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Sandhj/v4/main/menu/menu.sh" && chmod +x /usr/bin/menu
 wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/Sandhj/v4/main/menu/menu-ssh.sh" && chmod +x /usr/bin/menu-ssh
+wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/Sandhj/v4/main/backup/menu-backup.sh" && chmod +x /usr/bin/menu-backup
 chmod +x slow
 chmod +x menu-ssh
 chmod +x usernew
 chmod +x trial
+chmod +x menu-backuo
 chmod +x genssl
 echo -e " [INFO] Update Successfully"
 sleep 2
