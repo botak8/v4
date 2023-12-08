27dc5e43c22f221ec86b85032fbe725c1edc3dc6
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-09 02:46:12 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #wget https://github.com/${GitUser}/
-GitUser="Sandhj"
+GitUser="botak8"
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
@@ -35,25 +35,6 @@ sts="${Info2}"
 else
 sts="${Error}"
 fi
-clear
-echo ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$back_text                 \e[30m[\e[$box CHECK NEW UPDATE\e[30m ]                   \e[m"
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$below VVERSION NOW >> $Info1"
-echo -e "   \e[$below SSTATUS UPDATE >> $sts"
-echo -e ""
-echo -e "       \e[1;31mWould you like to proceed?\e[0m"
-echo ""
-echo -e "            \e[0;32m[ Select Option ]\033[0m"
-echo -e "     \e[$number [1]\e[m \e[$below CCheck Script Update Now\e[m"
-echo -e "     \e[$number [x]\e[m \e[$below BBack To Menu\e[m"
-echo -e ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "\e[$line"
-read -p "PPlease Choose 1 or x : " option2
-case $option2 in
-1)
 version=$(cat /home/ver)
 new_version=$( curl https://raw.githubusercontent.com/${GitUser}/v4/main/newversion | grep $version )
 if [ $version = $new_version ]; then
