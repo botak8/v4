2ffb4b07a7aa95185898a6da84073008947ba80e
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-09 13:37:46 +0800

    Update add-host.sh

--- ssh/add-host.sh
+++ ssh/add-host.sh
@@ -6,18 +6,21 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
 
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e "\e[33m ┌──────────────────────────────────┐\033[0m"
+echo -e "\e[33m │${cyan}.::::.  ADD DOMAIN SERVER  .::::.  \033[0m"
+echo -e "\e[33m └──────────────────────────────────┘\033[0m"
 echo ""
-read -rp "Domain/Host: " -e host
+read -rp "Domain/Host: " -e   host
 echo ""
 if [ -z $host ]; then
 echo "????"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[33m ────────────────────────────────────\033[0m"
 read -n 1 -s -r -p " Press any key to back on menu"
 menu
 else
 echo "IP=$host" > /var/lib/SIJA/ipvps.conf
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[33m ────────────────────────────────────\033[0m"
 echo " Pres enter to renew cert"
 echo ""
 read -n 1 -s -r -p "Press enter to renew cert"
