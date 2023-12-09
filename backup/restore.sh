ad3e2ae2b1477017599d0d3a14cbbfed69573564
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-09 23:03:48 +0800

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -28,33 +28,6 @@ rm -f backup.zip
 sleep 1
 echo "  Start Restore
 sleep 2
-fun_bar() {
-    CMD[0]="$1"
-    CMD[1]="$2"
-    (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${CMD[0]} -y >/dev/null 2>&1
-        ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
-    ) >/dev/null 2>&1 &
-    tput civis
-    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    while true; do
-        for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[0;32m#"
-            sleep 0.1s
-        done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[0;33m]"
-        sleep 1s
-        tput cuu1
-        tput dl1
-        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
-    done
-    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-    tput cnorm
-}
-res1() {
 cd /root/backup
 cp passwd /etc/
 cp group /etc/
@@ -70,10 +43,7 @@ cp -r shadowsocksr /usr/local/
 cp -r public_html /home/vps/
 cp crontab /etc/
 strt
-}
-netfilter-persistent
 clear
 rm -rf /root/backup
 rm -f backup.zip
-fun_bar 'res1'
 echo "  Restore Berhasil!!!" | lolcat
