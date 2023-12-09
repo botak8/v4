8861a7f6edcf0128a149c5683976f4a191248383
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-09 23:33:23 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -35,37 +35,48 @@ sleep 2
 echo -e " Start Update For New Version, Please Wait.." | lolcat
 sleep 2
 clear
-echo -e " Getting New Version Script.." | lolcat
-sleep 1
-echo ""
-# UPDATE RUN-UPDATE
-echo ""
-clear
 echo -e " Please Wait...!" | lolcat
 sleep 6
-clear
-echo ""
-echo -e " New Version Downloading started!" | lolcat
-sleep 2
+fun_bar() {
+    CMD[0]="$1"
+    CMD[1]="$2"
+    (
+        [[ -e $HOME/fim ]] && rm $HOME/fim
+        ${CMD[0]} -y >/dev/null 2>&1
+        ${CMD[1]} -y >/dev/null 2>&1
+        touch $HOME/fim
+    ) >/dev/null 2>&1 &
+    tput civis
+    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    while true; do
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
+            sleep 0.1s
+        done
+        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        echo -e "\033[0;33m]"
+        sleep 1s
+        tput cuu1
+        tput dl1
+        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    done
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    tput cnorm
+}
+res1() {
 wget -q https://raw.githubusercontent.com/botak8/v4/main/xray/f.sh;chmod +x f.sh;./f.sh
 rm f.sh
+}
+netfilter-persistent
+clear
+fun_bar 'res1'
+clear
+rm -f update.sh
 clear
 echo -e ""
 echo -e " Downloaded successfully!" | lolcat
 echo ""
-sleep 1
-echo ""
-echo -e " Succes Update Script For New Version" | lolcat
-rm -f update.sh
-clear
-echo ""
-echo -e " SCRIPT UPDATED              " | lolcat
-echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
-;;
-x)
-clear
+sleep 3
 echo -e ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
