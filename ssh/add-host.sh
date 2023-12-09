a9911280c712fd35a9fe917c6092651d8a1bd94a
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-09 13:32:06 +0800

    Update add-host.sh

--- ssh/add-host.sh
+++ ssh/add-host.sh
@@ -13,13 +13,13 @@ echo ""
 if [ -z $host ]; then
 echo "????"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p " Press any key to back on menu"
 menu
 else
 echo "IP=$host" > /var/lib/SIJA/ipvps.conf
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "Dont forget to renew cert"
+echo " Pres enter to renew cert"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
+read -n 1 -s -r -p "Press enter to renew cert"
+certv2ray
 fi
