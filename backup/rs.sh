a8b5de4d10764af76d8673f26da145097c3721df
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-12 03:34:50 +0800

    Create rs.sh

---
+++ backup/rs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+MYIP=$(wget -qO- ipinfo.io/ip);
+#
+echo -e "loading......" | lolcat
+                sleep 2
+                /etc/init.d/ssh restart
+                /etc/init.d/dropbear restart
+                /etc/init.d/stunnel4 restart
+                /etc/init.d/openvpn restart
+                /etc/init.d/fail2ban restart
+                /etc/init.d/cron restart
+                /etc/init.d/nginx restart
+                /etc/init.d/squid restart
+                echo -e "[ \033[1;36mOK\033[0m ] Restarting xray (via systemctl) "
+                systemctl restart xray
+                systemctl restart xray.service
+                echo -e "[ \033[1;36mOK\033[0m ] Restarting badvpn (via systemctl) "
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+                echo -e "[ \033[1;36mOK\033[0m ] Restart Websocket (via systemctl) "
+                systemctl restart sshws.service
+                systemctl restart ws-dropbear.service
+                systemctl restart ws-stunnel.service
+                echo -e "[ \033[1;36mOK\033[0m ] Restart Trojan Go (via systemctl) "
+                systemctl restart trojan-go.service 
+                echo -e "[ \033[1;32mSUCCES\033[0m ] \e[1;36mRestarted All Service\033[0m"
+                echo ""
+                read -n 1 -s -r -p "Press any key to back on system menu"
+                menu
