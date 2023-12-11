38b1da268d452a41a99b41b83ea1e3c585a71a74
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-11 15:37:08 +0800

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -47,4 +47,29 @@ echo -e "[ ${green}INFO${NC} ] Restart all service untuk mengaktipkan akun kemba
 sleep 2
 echo ""
 read -n 1 -s -r -p "Press any key to restart all service"
-restart
+                echo -e "Restart Begin" | lolcat
+                sleep 2
+                echo -e "loading......" | lolcat
+                sleep 2
+                /etc/init.d/ssh restart
+                /etc/init.d/dropbear restart
+                /etc/init.d/stunnel4 restart
+                /etc/init.d/openvpn restart
+                /etc/init.d/fail2ban restart
+                /etc/init.d/cron restart
+                /etc/init.d/nginx restart
+                /etc/init.d/squid restart
+                echo -e "[ \033[1;36mOK\033[0m ] Restarting xray (via systemctl) "
+                systemctl restart xray
+                systemctl restart xray.service
+                echo -e "[ \033[1;36mOK\033[0m ] Restarting badvpn (via systemctl) "
+                screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+                echo -e "[ \033[1;36mOK\033[0m ] Restart Websocket (via systemctl) "
+                systemctl restart sshws.service
+                systemctl restart ws-dropbear.service
+                systemctl restart ws-stunnel.service
+                echo -e "[ \033[1;36mOK\033[0m ] Restart Trojan Go (via systemctl) "
+                systemctl restart trojan-go.service 
+                echo -e "[ \033[1;32mSUCCES\033[0m ] \e[1;36mRestarted All Service\033[0m"
+                read -n 1 -s -r -p "Press any key to back on system menu"
+                menu
