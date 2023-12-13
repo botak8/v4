9cb0b1f20a3644e9cb712536c54d88bd7264cbaf
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-12 22:42:25 -0800

    Update del-tr.sh

--- xray/del-tr.sh
+++ xray/del-tr.sh
@@ -8,29 +8,26 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^#! " "/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}           • DELETE TROJAN USER •              ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}  • You Dont have any existing clients!"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}           ${GREEN} • SCRIPT MULTI PREMIUM •   $NC          $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+echo -e " $COLOR1│$NC   \e[33m     • DELETE TROJAN ACOUNT •       ${NC}"
+echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 │$NC   \e[33m     • You Dont have any existing clients!"
+echo -e "$COLOR1 └─────────────────────────────────────────┘${NC}" 
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 menu-trojan
 fi
 clear
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}           • DELETE TROJAN USER •              ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+echo -e " $COLOR1│$NC   \e[33m     • DELETE TROJAN ACOUNT •       ${NC}"
+echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1 ┌──────────────────────────────────────────┐${NC}"
 grep -E "^#! " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq | nl
-echo -e "$COLOR1│${NC}"
-echo -e "$COLOR1│${NC}  • [NOTE] Press any key to back on menu"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1───────────────────────────────────────────────────${NC}"
+echo -e "$COLOR1 │${NC}"
+echo -e "$COLOR1 │${NC}     • [NOTE] Press any key to back on menu"
+echo -e "$COLOR1 └──────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1 ────────────────────────────────────────────${NC}"
 read -rp "   Input Username : " user
 if [ -z $user ]; then
 menu-trojan
@@ -39,18 +36,15 @@ exp=$(grep -wE "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | un
 sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
 systemctl restart xray > /dev/null 2>&1
 clear
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC} ${COLBG1}           • DELETE TROJAN USER •              ${NC} $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}   • Accound Delete Successfully"
-echo -e "$COLOR1│${NC}"
-echo -e "$COLOR1│${NC}   • Client Name : $user"
-echo -e "$COLOR1│${NC}   • Expired On  : $exp"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
-echo -e "$COLOR1┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "$COLOR1│${NC}           ${GREEN} • SCRIPT MULTI PREMIUM •   $NC          $COLOR1│$NC"
-echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}" 
+echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+echo -e " $COLOR1│$NC   \e[33m     • DELETE TROJAN ACOUNT •       ${NC}"
+echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1 ┌─────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Accound Delete Successfully${NC}"
+echo -e "$COLOR1 │${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Client Name : $user ${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Expired On  : $exp ${NC}"
+echo -e "$COLOR1 └─────────────────────────────────────────┘${NC}" 
 echo ""
 read -n 1 -s -r -p "   Press any key to back on menu"
 menu-trojan
