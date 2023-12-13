0b6d559125170c22e69cb33e8b6acf001e6c3ad0
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-12 22:56:58 -0800

    Update del-ws.sh

--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -19,17 +19,22 @@ MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
 NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo ""
-		echo "You have no existing clients!"
-		exit 1
-	fi
+		echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+                echo -e " $COLOR1│$NC   \e[33m     • DELETE VMESS ACOUNT •       ${NC}"
+                echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+                echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+                echo -e "$COLOR1 │$NC   \e[33m     • You Dont have any existing clients!"
+                echo -e "$COLOR1 └─────────────────────────────────────────┘${NC}" 
+                echo ""
+read -n 1 -s -r -p "   Press any key to back on menu"
+        menu-vmess
 
 	clear
-	echo ""
-	echo " Select the existing client you want to remove"
-	echo " Press CTRL+C to return"
-	echo " ==============================="
-	echo "     No  Expired   User"
+	clear
+	echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+        echo -e " $COLOR1│$NC   \e[33m     • DELETE VMESS ACOUNT •       ${NC}"
+        echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+    echo "   User       Expired  " 
 	grep -E "^### " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
@@ -45,14 +50,16 @@ sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
 rm -f /etc/xray/vmess-$user-tls.json /etc/xray/vmess-$user-nontls.json
 systemctl restart xray.service
 clear
+echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+echo -e " $COLOR1│$NC   \e[33m     • DELETE VMESS ACOUNT •       ${NC}"
+echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1 ┌─────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Accound Delete Successfully${NC}"
+echo -e "$COLOR1 │${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Client Name : $user ${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Expired On  : $exp ${NC}"
+echo -e "$COLOR1 └─────────────────────────────────────────┘${NC}" 
 echo ""
-echo "==============================="
-echo "  Xray/Vmess Account Deleted  "
-echo "==============================="
-echo "Username  : $user"
-echo "Expired   : $exp"
-echo "==============================="
-echo "Script By ARTA M"
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
-
+read -n 1 -s -r -p "   Press any key to back on menu"
+menu-vmess
+    
