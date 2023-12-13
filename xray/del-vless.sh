2afe7c052e61fe23c81eff6cc71308b2aefd5995
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-12 22:49:45 -0800

    Update del-vless.sh

--- xray/del-vless.sh
+++ xray/del-vless.sh
@@ -12,28 +12,28 @@ clear
 
 NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/config.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-        echo -e "\E[40;1;37m     ⇱ Delete Vless Account ⇲      \E[0m"
-        echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		echo ""
-		echo "You have no existing clients!"
-		echo ""
-		echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-		read -n 1 -s -r -p "Press any key to back on menu"
+		echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+                echo -e " $COLOR1│$NC   \e[33m     • DELETE VLESS ACOUNT •       ${NC}"
+                echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+                echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+                echo -e "$COLOR1 │$NC   \e[33m     • You Dont have any existing clients!"
+                echo -e "$COLOR1 └─────────────────────────────────────────┘${NC}" 
+                echo ""
+read -n 1 -s -r -p "   Press any key to back on menu"
         menu
 	fi
 
 	clear
-	echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo -e "\E[40;1;37m     ⇱ Delete Vless Account ⇲      \E[0m"
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo "  User       Expired  " 
-	echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+	echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+        echo -e " $COLOR1│$NC   \e[33m     • DELETE VLESS ACOUNT •       ${NC}"
+        echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+    echo "   User       Expired  " 
+	echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
 	grep -E "^#& " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
     echo ""
     red "tap enter to go back"
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-	read -rp "Input Username : " user
+    echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+	read -rp "  Input Username : " user
     if [ -z $user ]; then
     menu
     else
@@ -41,13 +41,16 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#& " "/etc/xray/config.json")
     sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
     systemctl restart xray > /dev/null 2>&1
     clear
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " VLess Account Deleted Successfully"
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo " Client Name : $user"
-    echo " Expired On  : $exp"
-    echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-    echo ""
-    read -n 1 -s -r -p "Press any key to back on menu"
-    menu
+echo -e " $COLOR1┌─────────────────────────────────────────┐${NC}"
+echo -e " $COLOR1│$NC   \e[33m     • SCESS DELETE TROJAN ACOUNT •       ${NC}"
+echo -e " $COLOR1└─────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1 ┌─────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Accound Delete Successfully${NC}"
+echo -e "$COLOR1 │${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Client Name : $user ${NC}"
+echo -e "$COLOR1 │${NC} \e[33m    • Expired On  : $exp ${NC}"
+echo -e "$COLOR1 └─────────────────────────────────────────┘${NC}" 
+echo ""
+read -n 1 -s -r -p "   Press any key to back on menu"
+menu-vless
     fi
