3a1a61e39131a1ac087a9b62647f11c7c59cbf2d
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-13 15:57:20 +0800

    Update f.sh

--- xray/f.sh
+++ xray/f.sh
@@ -2,31 +2,50 @@
 
 cd /usr/bin
 # vmess
-wget -O add-ws "https://raw.githubusercontent.com/heruahmad1/v4/main/menu/add2.sh" && chmod +x add-ws
-wget -O add-vless "https://raw.githubusercontent.com/heruahmad1/v4/main/menu/add3.sh" && chmod +x add-vless
-wget -O add-tr "https://raw.githubusercontent.com/heruahmad1/v4/main/menu/add4.sh" && chmod +x add-tr
-wget -O addtrgo "https://raw.githubusercontent.com/sasak3/v4/main/xray/addtrgo.sh" && chmod +x addtrgo
-wget -O trialtrojango "https://raw.githubusercontent.com/sasak3/v4/main/xray/trialtrojango.sh" && chmod +x trialtrojango
-wget -O deltrgo "https://raw.githubusercontent.com/sasak3/v4/main/xray/deltrgo.sh" && chmod +x deltrgo
-wget -O renewtrgo "https://raw.githubusercontent.com/sasak3/v4/main/xray/renewtrgo.sh" && chmod +x renewtrgo
-wget -O cektrgo "https://raw.githubusercontent.com/sasak3/v4/main/xray/cektrgo.sh" && chmod +x cektrgo
+wget -O add-ws "https://raw.githubusercontent.com/botak8/v4/main/xray/add-ws.sh" && chmod +x add-ws
+wget -O trialvmess "https://raw.githubusercontent.com/botak8/v4/main/xray/trialvmess.sh" && chmod +x trialvmess
+wget -O renew-ws "https://raw.githubusercontent.com/botak8/v4/main/xray/renew-ws.sh" && chmod +x renew-ws
+wget -O del-ws "https://raw.githubusercontent.com/botak8/v4/main/xray/del-ws.sh" && chmod +x del-ws
+wget -O cek-ws "https://raw.githubusercontent.com/botak8/v4/main/xray/cek-ws.sh" && chmod +x cek-ws
+
+# vless
+wget -O add-vless "https://raw.githubusercontent.com/botak8/v4/main/xray/add-vless.sh" && chmod +x add-vless
+wget -O trialvless "https://raw.githubusercontent.com/botak8/v4/main/xray/trialvless.sh" && chmod +x trialvless
+wget -O renew-vless "https://raw.githubusercontent.com/botak8/v4/main/xray/renew-vless.sh" && chmod +x renew-vless
+wget -O del-vless "https://raw.githubusercontent.com/botak8/v4/main/xray/del-vless.sh" && chmod +x del-vless
+wget -O cek-vless "https://raw.githubusercontent.com/botak8/v4/main/xray/cek-vless.sh" && chmod +x cek-vless
+
+# trojan
+wget -O add-tr "https://raw.githubusercontent.com/botak8/v4/main/xray/add-tr.sh" && chmod +x add-tr
+wget -O trialtrojan "https://raw.githubusercontent.com/botak8/v4/main/xray/trialtrojan.sh" && chmod +x trialtrojan
+wget -O del-tr "https://raw.githubusercontent.com/botak8/v4/main/xray/del-tr.sh" && chmod +x del-tr
+wget -O renew-tr "https://raw.githubusercontent.com/botak8/v4/main/xray/renew-tr.sh" && chmod +x renew-tr
+wget -O cek-tr "https://raw.githubusercontent.com/botak8/v4/main/xray/cek-tr.sh" && chmod +x cek-tr
+
+# trojan go
+wget -O addtrgo "https://raw.githubusercontent.com/botak8/v4/main/xray/addtrgo.sh" && chmod +x addtrgo
+wget -O trialtrojango "https://raw.githubusercontent.com/botak8/v4/main/xray/trialtrojango.sh" && chmod +x trialtrojango
+wget -O deltrgo "https://raw.githubusercontent.com/botak8/v4/main/xray/deltrgo.sh" && chmod +x deltrgo
+wget -O renewtrgo "https://raw.githubusercontent.com/botak8/v4/main/xray/renewtrgo.sh" && chmod +x renewtrgo
+wget -O cektrgo "https://raw.githubusercontent.com/botak8/v4/main/xray/cektrgo.sh" && chmod +x cektrgo
+
 # menu
 wget -O menu "https://raw.githubusercontent.com/botak8/v4/main/menu/menu.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/menu2.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/menu3.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/gas2.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/gas3.sh"
 wget -O running "https://raw.githubusercontent.com/botak8/v4/main/menu/running.sh"
 wget -O clearcache "https://raw.githubusercontent.com/botak8/v4/main/menu/clearcache.sh"
-wget -O menu-trgo "https://raw.githubusercontent.com/botak8/v4/main/menu/menu-trgo.sh"
-wget -O menu-trojan "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/menu4.sh"
+wget -O menu-trgo "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/gas5.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/gas4.sh"
 # menu ssh ovpn
-wget -O menu-ssh "https://raw.githubusercontent.com/botak8/v4/main/menu/menu-ssh.sh"
-wget -O usernew "https://raw.githubusercontent.com/sasak3/v4/main/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/sasak3/v4/main/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/sasak3/v4/main/ssh/renew.sh"
+wget -O menu-ssh "https://raw.githubusercontent.com/heruahmad1/backupvpshrvpn/main/gas1.sh"
+wget -O usernew "https://raw.githubusercontent.com/botak8/v4/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/botak8/v4/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/botak8/v4/main/ssh/renew.sh"
 wget -O hapus "https://raw.githubusercontent.com/sasak3/v4/main/ssh/hapus.sh"
 wget -O cek "https://raw.githubusercontent.com/sasak3/v4/main/ssh/cek.sh"
 wget -O member "https://raw.githubusercontent.com/sasak3/v4/main/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/sasak3/v4/main/ssh/delete.sh"
+wget -O delete "https://raw.githubusercontent.com/botak8/v4/main/ssh/delete.sh"
 wget -O autokill "https://raw.githubusercontent.com/botak8/v4/main/ssh/autokill.sh"
 wget -O ceklim "https://raw.githubusercontent.com/botak8/v4/main/ssh/ceklim.sh"
 wget -O tendang "https://raw.githubusercontent.com/botak8/v4/main/ssh/tendang.sh"
