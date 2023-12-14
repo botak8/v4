9a370e8e63ad097fcd0fe6f00394a548e3880e8a
botak8 <153253934+botak8@users.noreply.github.com>
2023-12-14 20:02:04 +0800

    Update roy.sh

--- xray/roy.sh
+++ xray/roy.sh
@@ -1,18 +1,6 @@
-clear
-echo ""
-  echo -e "\e[32m      ┌───────────────────────────────────────────────┐\033[0m"
-  echo -e "\e[32m   ───│                                               │───\033[0m"
-  echo -e "\e[32m   ───│    ┌─┐┬ ┬┌┬┐┌─┐┌─┐┌─┐┬─┐┬┌─┐┌┬┐  ┬  ┬┌┬┐┌─┐   │───\033[0m"
-  echo -e "\e[32m   ───│    ├─┤│ │ │ │ │└─┐│  ├┬┘│├─┘ │   │  │ │ ├┤    │───\033[0m"
-  echo -e "\e[32m   ───│    ┴ ┴└─┘ ┴ └─┘└─┘└─┘┴└─┴┴   ┴   ┴─┘┴ ┴ └─┘   │───\033[0m"
-  echo -e "\e[32m      │\033[0m  \e[33m      HR-vpn (C)https://t.me/HRstores      \033[0m \e[32m │\033[0m"
-  echo -e "\e[32m      └───────────────────────────────────────────────┘\033[0m"
-  echo -e "\e[36m             Autoscript xray vpn lite (multi port)\033[0m"
-  echo -e "\e[36m  Make sure the internet is smooth when installing the script\033[0m"
-  echo -e "\e[31m     JANGAN INSTALL SCRIPT INI MENGGUNAKAN KONEKSI VPN!!!\033[0m"
-  echo ""
-read -n 1 -s -r -p " Tekan enter untuk melanjutkan"
-clear
+#!/bin/bash
+
+
 # Install dep
 apt install -y wget screen
 apt update -y && apt upgrade -y
@@ -20,6 +8,6 @@ apt install lolcat -y
 gem install lolcat
 
 # Download and run setup.sh
-wget -q https://raw.githubusercontent.com/botak8/san/main/getting.sh
-chmod +x getting.sh
-screen -S install ./getting.sh
+wget -q https://raw.githubusercontent.com/botak8/san/main/hr.sh
+chmod +x hr.sh
+screen -S install ./hr.sh
